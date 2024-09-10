5d2706674c72d657ebadac3ba049dce1a854f2bf
Yudhi <yudhioktaviangule@gmail.com>
2024-09-10 20:45:24 +0700

    update NamespacesEnum.php

--- src/Enums/NamespacesEnum.php
+++ src/Enums/NamespacesEnum.php
@@ -5,5 +5,5 @@ namespace Yudhi\Apigen\Enums;
 enum NamespacesEnum: string
 {
     case Base = '{rootNamespace}\\Core\\{entityName}';
-    case Request = '{rootNamespace}\\{entityName}\\HttpRequests';
+    case Request = '{rootNamespace}\\Core\\{entityName}\\HttpRequests';
 }
