9e943a4df8a19e36c10a0b1c11e1c2e5b1c9bc5f
Yudhi <yudhioktaviangule@gmail.com>
2024-09-10 14:00:09 +0700

    update config file

--- src/Config/apigen.php
+++ src/Config/apigen.php
@@ -1,5 +1,19 @@
 <?php
-
-return [
-
+return
+    [
+        'abstractControllerName' => 'App\\Http\\Controllers\\Controller',
+        'rootNamespace' => 'App',
+        'basePath' => base_path(),
+        'appPath' => app_path(),
+        'stubPath' => [
+            'controller' => '{rootPath}'.DIRECTORY_SEPARATOR.'stubs'.DIRECTORY_SEPARATOR.'controller.stub',
+            'entity' => '{rootPath}'.DIRECTORY_SEPARATOR.'stubs'.DIRECTORY_SEPARATOR.'entity.stub',
+            'repository' => '{rootPath}'.DIRECTORY_SEPARATOR.'stubs'.DIRECTORY_SEPARATOR.'repository.stub',
+            'service' => '{rootPath}'.DIRECTORY_SEPARATOR.'stubs'.DIRECTORY_SEPARATOR.'service.stub',
+            'requests' => [
+                'base' => '{rootPath}'.DIRECTORY_SEPARATOR.'stubs'.DIRECTORY_SEPARATOR.'request.base.stub',
+                'create' => '{rootPath}'.DIRECTORY_SEPARATOR.'stubs'.DIRECTORY_SEPARATOR.'request.create.stub',
+                'update' => '{rootPath}'.DIRECTORY_SEPARATOR.'stubs'.DIRECTORY_SEPARATOR.'request.update.stub',
+            ],
+        ]
 ];
