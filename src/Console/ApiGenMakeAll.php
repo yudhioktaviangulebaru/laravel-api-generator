8867facda430f559ee46e00e999a94ef234f6474
Yudhi <yudhioktaviangule@gmail.com>
2024-09-10 09:20:34 +0700

    - remove unused command - add command to generate all

---
+++ src/Console/ApiGenMakeAll.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Yudhi\Apigen\Console;
+
+use Illuminate\Console\Command;
+use Yudhi\Apigen\Core\ApiGenerator;
+
+class ApiGenMakeAll extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'api-generate:all {module}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create set API';
+
+    private string $module = '';
+    public function handle()
+    {
+        $this->module = $this->argument('module') ?? '';
+        $this->setModule();
+        ApiGenerator::make($this->module)->generateAll();
+
+    }
+
+    private function setModule(): void
+    {
+        if($this->module==='') {
+            $this->module = $this->ask("Please specify Module Name") ?? '';
+        }
+        if($this->module==='') {
+            $this->setModule();
+        }
+    }
+}
